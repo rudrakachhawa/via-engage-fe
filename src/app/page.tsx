1fcd88da2cb1188867c14f738535a3a8ae52321c
Rudraksh <rudrakshkachhawa@gmail.com>
2026-05-05 23:54:09 +0530

    fix sanity issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,9 @@ export default function Home() {
         <button className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
           onClick={() => {
             const popup = window.open("", "popup", "width=600,height=800");
-            popup.location.href = "https://www.instagram.com/oauth/authorize?force_reauth=true&client_id=2187891761969078&redirect_uri=https://jaws-wildcard-donor.ngrok-free.dev/oauth&response_type=code&scope=instagram_business_basic%2Cinstagram_business_manage_messages%2Cinstagram_business_manage_comments%2Cinstagram_business_content_publish%2Cinstagram_business_manage_insights";
+            if (popup) {
+              popup.location.href = "https://www.instagram.com/oauth/authorize?force_reauth=true&client_id=2187891761969078&redirect_uri=https://jaws-wildcard-donor.ngrok-free.dev/oauth&response_type=code&scope=instagram_business_basic%2Cinstagram_business_manage_messages%2Cinstagram_business_manage_comments%2Cinstagram_business_content_publish%2Cinstagram_business_manage_insights";
+            }
           }}>
           Connect with Instagram
         </button>
