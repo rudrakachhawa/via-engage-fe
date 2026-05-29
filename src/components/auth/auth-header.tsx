903ccd96c4505f1480ecdf4048d7b4cc2834d389
Rudraksh <rudrakshkachhawa@gmail.com>
2026-05-29 17:58:29 +0530

    replace old branding icon on login page

--- src/components/auth/auth-header.tsx
+++ src/components/auth/auth-header.tsx
@@ -7,24 +7,30 @@ export function AuthHeader() {
           text-center
         "
         >
-
-            <h1
-                className="
-            text-3xl font-bold
-            tracking-tight
-            text-primary
-          "
-            >
-                ezzzDM
-            </h1>
-
+            <div className="flex items-center justify-center mb-1 gap-2">
+                <img
+                    src="/appIcon.png"
+                    alt="Logo"
+                    width={70}
+                    height={70}
+                />
+                <h1
+                    className="
+                    text-3xl font-bold
+                    tracking-tight
+                    text-primary
+                  "
+                >
+                    ezzzDM
+                </h1>
+            </div>
             <p
                 className="
-            mt-3
-            max-w-[320px]
-            text-base leading-7
-            text-muted-foreground
-          "
+                mt-3
+                max-w-[320px]
+                text-base leading-7
+                text-muted-foreground
+              "
             >
                 Automate your Instagram engagement
                 with enterprise-grade precision.
